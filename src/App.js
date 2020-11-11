b7fec2e36f55080471588bb12f2cd08604142352
rsouzajunior <robertosouzajunior@outlook.com>
2020-11-11 19:36:33 -0300

    c 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class Origin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //  uid: props.match.params.uid,
+      getcategory: [],
+
+    }
+  }
+
+  componentDidMount() {
+    const api012 = "/api/getcategory/Vxgia2VqxUY8Tt7Xycg3yIhSzSP2"
+    console.log('api ' + api012)
+
+    fetch(api012)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ getcategory: data })
+      }).catch((error) => { console.log('erro: ' + error) });
+
+  }
+
+  render() {
+    console.log(this.state.getcategory)
+    return (
+      <div className="App">
+        <div className="App-header">
+          API
+          <br />
+          {this.state.getcategory.map(get =>  (get.name))}
+        </div>
+      </div>
+
+    )
+  }
+
 }
 
-export default App;
